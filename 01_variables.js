f20f2f4303f89f5fabd639b20da21fe6c7a081aa
VaibhavRajput91 <99875095+VaibhavRajput91@users.noreply.github.com>
2024-05-27 17:18:40 +0530

    Add files via upload

---
+++ 01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 14456
+let accountEmail = "vaibh@gmail.com"
+var acountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 14459
+
+/*
+do not use var. Always prefer let or const
+because of issues in block scope and functional scope
+*/
+console.table([accountId, accountEmail, acountPassword, accountCity, accountState])
